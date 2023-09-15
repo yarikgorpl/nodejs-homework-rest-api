4d4c06f3f318dca698ea8f88d7c63f6d18552939
Yaroslav Protsiuk <jaroslav.procjuk@gmail.com>
2023-09-16 00:31:03 +0300

    mongoose installed

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const app = require('./app')
+// c1Gvu3A1QBm6uaDu
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require("mongoose");
+
+const DB_HOST =
+  "mongodb+srv://Yaroslav:c1Gvu3A1QBm6uaDu@cluster0.mhhk43t.mongodb.net/contacts_list?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
